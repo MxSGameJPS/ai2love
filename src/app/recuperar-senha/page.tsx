227d222c486b49e9fde61f96628dbfc15a79ed44
MxSGameJPS <mxsgamejps@gmail.com>
2025-05-08 17:43:07 -0300

    Atualiza a página de "recuperar-senha" removendo a importação desnecessária do componente de imagem e ajustando a formatação do código. Inclui melhorias no rastreamento de desempenho para a nova página.

--- src/app/recuperar-senha/page.tsx
+++ src/app/recuperar-senha/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function RecuperarSenha() {
   const [email, setEmail] = useState("");
@@ -133,4 +132,4 @@ export default function RecuperarSenha() {
       </div>
     </div>
   );
-} 
+}
