5b8b595c7597d3aeb40c26b2648b71c0fdbe4a6f
MxSGameJPS <mxsgamejps@gmail.com>
2025-05-08 21:18:49 -0300

    Atualiza manifestos de construção e arquivos estáticos, incluindo melhorias no rastreamento de desempenho e ajustes em várias páginas do projeto, como login e erro. Adiciona novos chunks e otimizações para o carregamento de imagens.

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nS8KsuBtC4mAscpJ1LEJIdQwjPlcKGNqEfMO0XdPkiY=\"\n}"
--- .next/static/chunks/src_app_page_tsx_781e1e16._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_f5074bf6._.js",
-  "static/chunks/src_components_bf0c8455._.js"
-],
-    source: "dynamic"
-});
